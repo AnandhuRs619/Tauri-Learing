af796e70595145d10a98dc74cd064e8bf5c782f0
AnandhuRs619 <ananthurs619@gmail.com>
2025-02-24 21:19:19 +0530

    basic config

---
+++ postcss.config.cjs
@@ -0,0 +1,6 @@
+module.exports = {
+  plugins: {
+    '@tailwindcss/postcss': {}, // Use the new Tailwind CSS PostCSS plugin
+    autoprefixer: {}, // Add autoprefixer for browser compatibility
+  },
+};
